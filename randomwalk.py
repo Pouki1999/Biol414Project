669f09c326d2e3075ed9185a595abeca5e7ae578
Pouki1999 <gogo.maslyankov@gmail.com>
2022-11-23 08:46:01 -0500

    adding obstacles

--- randomwalk.py
+++ randomwalk.py
@@ -1,26 +1,23 @@
 from tkinter import Tk, BOTH, Frame, Canvas
 import numpy as np
 import math
-import pynput.keyboard
-
 
 """
 class Ant:
-    def __init__(self):        
+    def __init__(self):
+        pass
 """
 
-
 class Environment(Frame):
-
     def __init__(self):
         super().__init__()
         self.canvas = Canvas(self)
+        self.obstacles = [(100, 300, 400, 500), (450, 50, 550, 500), (700, 100, 950, 450)]
+        self.food_sources = [(50, 100), (600, 500), (900, 40)]
+        self.nest_pos = (300.0, 250.0)
+        self.curr_pos = self.nest_pos
+        self.limits = (1000, 600)
         self.initUI()
-        self.obstacles = []
-        self.limits = [1000, 400]
-        self.bind("<KeyPress>", self.keydown)
-        self.bind("<KeyRelease>", self.keyup)
-        self.curr_pt = (200.0, 100.0)
 
         self.pack()
         self.focus_set()
@@ -33,35 +30,74 @@ class Environment(Frame):
         self.canvas.create_line(300, 35, 300, 200, dash=(4, 2))
         self.canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
         """
+        self.canvas.create_oval(self.nest_pos[0] - 3, self.nest_pos[1] - 3, self.nest_pos[0] + 3, self.nest_pos[1] + 3)
+
+        for o in self.obstacles:
+            self.canvas.create_rectangle(o[0], o[1], o[2], o[3])
+
+        for f in self.food_sources:
+            self.canvas.create_oval(f[0] - 3, f[1] - 3, f[0] + 3, f[1] + 3)
+
+
+        self.bind("<KeyPress>", self.keydown)
+        self.bind("<KeyRelease>", self.keyup)
         self.canvas.pack(fill=BOTH, expand=1)
 
     def keyup(self, e):
         pass
 
     def keydown(self, e):
-        if e.char == 'w':
-            self.curr_pt = ()
-        else if e.char == 'd':
-
-        else if e.char == 's':
-
-        else if e.char == 's':
-
-        else if e.char == '':
-
-
-        print(e.char)
+        self.random_walk_next_step(3)
 
     def random_walk_next_step(self, step):
         while True:
             angle = np.random.uniform(0.0, 2 * math.pi, size=None)
-            new_x = self.curr_pt[0] + int(step * step * math.cos(angle))
-            new_y = self.curr_pt[1] + int(step * step * math.sin(angle))
+            new_x = self.curr_pos[0] + int(step * step * math.cos(angle) + 0.5)
+            new_y = self.curr_pos[1] + int(step * step * math.sin(angle) + 0.5)
+            bad = False
+            for o in self.obstacles:
+                if inRect((new_x, new_y), o) or lineHitsRect((self.curr_pos[0], self.curr_pos[1]), (new_x, new_y), o):
+                    bad = True
+            if bad:
+                continue
             if 0 <= new_x <= self.limits[0] and 0 <= new_y <= self.limits[1]:
                 break
-        self.canvas.create_line(self.curr_pt[0], self.curr_pt[1], new_x, new_y)
-        self.curr_pt = [new_x, new_y]
+        self.canvas.create_line(self.curr_pos[0], self.curr_pos[1], new_x, new_y)
+        self.curr_pos = (new_x, new_y)
 
+
+def inRect(p, rect):
+    """ Return 1 in p is inside rect, dilated by dilation (for edge cases). """
+    if rect[2] >= p[0] >= rect[0] and rect[3] >= p[1] >= rect[1]:
+        return True
+    else:
+        return False
+
+
+def lineHitsRect(p1, p2, r):
+    if (p1[0] - p2[0]) == 0:
+        m = 100000
+    else:
+        m = (p1[1] - p2[1])/(p1[0] - p2[0])
+    b = p1[1] - m*p1[0]
+    y1 = m*r[0] + b
+    y2 = m*r[2] + b
+    if m != 0:
+        x1 = (r[1] - b) / m
+        x2 = (r[3] - b) / m
+    else:
+        x1 = 100000
+        x2 = 100000
+
+    if (p1[0] <= r[0] <= p2[0] or p2[0] <= r[0] <= p1[0]) and (r[1] <= y1 <= r[3]):
+        return True
+    if (p1[0] <= r[2] <= p2[0] or p2[0] <= r[2] <= p1[0]) and (r[1] <= y2 <= r[3]):
+        return True
+    if (r[0] <= x1 <= r[2]) and (p1[1] <= r[1] <= p2[1] or p2[1] <= r[1] <= p1[1]):
+        return True
+    if (r[0] <= x2 <= r[2]) and (p1[1] <= r[3] <= p2[1] or p2[1] <= r[3] <= p1[1]):
+        return True
+    return False
 
 def main():
     root = Tk()
@@ -69,6 +105,7 @@ def main():
     limits_string = str(env.limits[0]) + "x" + str(env.limits[1])
     root.geometry(limits_string)
     root.mainloop()
+    print('we are in the main loop')
 
 
 if __name__ == '__main__':
---
+++ test_georgi.py
@@ -0,0 +1,12 @@
+from tkinter import Tk
+
+
+def main():
+    root = Tk()
+    print('before loop')
+    root.mainloop()
+    print('we are in the main loop')
+
+
+if __name__ == '__main__':
+    main()
